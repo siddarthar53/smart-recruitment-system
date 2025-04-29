79764bb78849141e90357f7ff4d3f2d5548e55b5
Siddartha Reddy Madgula <madgulasiddarthareddy@gmail.com>
2025-04-29 10:00:48 +0530

    removed unwanted files

--- back-end/deleteFileS3.js
+++
@@ -1,8 +0,0 @@
-const { deleteFromS3 } = require("./s3Bucket"); // Import the delete function
-
-const bucketName = "sid-major-bucket"; 
-const fileKey = "jobDescriptions/project_manager.pdf"; // Path inside S3
-
-deleteFromS3(bucketName, fileKey)
-  .then(response => console.log(response))
-  .catch(error => console.error("Error:", error));
